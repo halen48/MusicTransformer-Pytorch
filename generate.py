6109fa27968d24bc68b51e1ddb9c24e5bad35818
Guilherme Novaes <gui.novaes94@outlook.com>
2021-09-02 03:04:07 -0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -64,8 +64,7 @@ def main():
 
         
     if(args.model == "lstm"):
-        model = MusicLSTM(n_layers=args.n_layers, 
-            d_model=args.d_model, 
+        model = MusicLSTM(d_model=args.d_model, 
             max_sequence=args.max_sequence).to(get_device())
     elif(args.model == "transformer"):
         model = MusicTransformer(n_layers=args.n_layers, num_heads=args.num_heads,
